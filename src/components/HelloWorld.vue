a1999a7dd5b503651040ddffdf20fb8ea6edf936
zz bct <1668856072@qq.com>
2017-11-30 18:10:55 +0800

    proxy server

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <input class="form-control" id="inputEmail3" placeholder="请输入账号" v-model="account">
+    <input type="password" class="form-control" id="inputPassword3" placeholder="请输入密码" v-model="password">
+    <button type="submit" class="btn btn-default" @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        account: '',
+        password: ''
+      }
+    },
+    methods:{
+      login() {
+        // 获取已有账号密码
+        this.$http.get('/server/users')
+          .then((response) => {
+            // 响应成功回调
+            console.log(response.body);
+          })
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((reject) => {
+            console.log(reject)
+          });
+      }
+    }
+  }
+</script>
